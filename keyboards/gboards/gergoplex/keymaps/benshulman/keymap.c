5a7c99d29e4fb9822caba06e65509614c44a947b
Ben Shulman <bshulman@indigoag.com>
2021-09-29 14:47:33 -0400

    Set left shift for nav

--- keyboards/gboards/gergoplex/keymaps/benshulman/keymap.c
+++ keyboards/gboards/gergoplex/keymaps/benshulman/keymap.c
@@ -85,15 +85,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |-------+-----+-----+-----+-----|      |-----+-----+-----+-----+-------|
      * |  F1   | F2  | F3  | F4  | F5  |      | LFT | DWN | UP  | RGT | VOLUP |
      * |-------+-----+-----+-----+-----|      |-----+-----+-----+-----+-------|
-     * |  F6   | F7  | F8  | F9  | F10 |      |SH-LF|SH-DW|SH-UP|SH-RG| VOLDN |
+     * |F6 SHFT| F7  | F8  | F9  | F10 |      |SH-LF|SH-DW|SH-UP|SH-RG| VOLDN |
      * `-------------------------------'      `-------------------------------'
      *             .--------------------.          .-----------------.
      *             | ESC META | ENT ALT |          |     | PLY | SKP |
      *             '--------------------'          '-----------------'
      */
     [_NUMBERS] = LAYOUT_split_3x5_3(
-         KC_1,  KC_2,  KC_3,  KC_4,  KC_5,           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-         KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_VOLU,
-         KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,         LSFT(KC_LEFT), LSFT(KC_DOWN), LSFT(KC_UP),   LSFT(KC_RGHT), KC_VOLD,
-            KC_GUI_ESC, KC_ALT_ENT, KC_TRNS,         KC_TRNS,  KC_MPLY,  KC_MNXT)
+         KC_1,                KC_2,  KC_3,  KC_4,  KC_5,           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+         KC_F1,               KC_F2, KC_F3, KC_F4, KC_F5,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_VOLU,
+         MT(MOD_LSFT, KC_F6), KC_F7, KC_F8, KC_F9, KC_F10,         LSFT(KC_LEFT), LSFT(KC_DOWN), LSFT(KC_UP),   LSFT(KC_RGHT), KC_VOLD,
+            KC_GUI_ESC, KC_ALT_ENT, KC_TRNS,                       KC_TRNS,  KC_MPLY,  KC_MNXT)
 };
